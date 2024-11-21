a1e57a04f8236fc3d1f277587547cb1e7af709c4
COMBA Yannick <comba.yannick@gmail.com>
2024-11-21 10:54:41 +0100

    common password

--- app/Http/Controllers/FunctionHackerController.php
+++ app/Http/Controllers/FunctionHackerController.php
@@ -34,14 +34,7 @@ class FunctionHackerController extends Controller
      *         @OA\JsonContent(
      *             @OA\Property(property="email", type="string", format="email", example="test@example.com")
      *         ),
-     *     ),
-     *     @OA\Response(
-     *         response=401,
-     *         description="L'email n'existe pas",
-     *         @OA\JsonContent(
-     *             @OA\Property(property="error", type="string", example="L'email n'existe pas")
-     *         )
-     *     ),
+     *     )
      * )
      */
     public function EmailVerificator()
@@ -63,5 +56,60 @@ class FunctionHackerController extends Controller
         ]);
 
         return response()->json($response->json());
+    }
+
+    /**
+     * @OA\Post(
+     *     path="auth/common-password",
+     *     summary="Vérifie si le mot de passe fait partie des mots de passe les plus fréquents",
+     *     description="Vérifie si le mot de passe fait partie des mots de passe les plus fréquents",
+     *     tags={"password"},
+     * @OA\Parameter(
+     *         name="bearerToken",
+     *         in="query",
+     *         description="Token de connexion de l'utilisateur",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             format="bearer",
+     *             example="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTg1Ljk4LjEzOC41Ni9hcGkvYXV0aC9sb2dpbiIsImlhdCI6MTczMDM4OTU1NSwiZXhwIjoxNzMwMzkzMTU1LCJuYmYiOjE3MzAzODk1NTUsImp0aSI6IjNUYmNnWHIxMjNDaGVpVkQiLCJzdWIiOiIxIiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.do2Iv4GOl28ppLio4W3u1BF30_WoBA5E9nEae7ULwGQ"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Le mot de passe est utilisé",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="string", type="string", format="string", example="test")
+     *         ),
+     *     )
+     * )
+     */
+    public function getCommonPasswords()
+    {
+        if (!Auth::id())
+        {
+            CommonUtilitary::UserNotConnected("getCommonPasswords");
+
+            return response()->json(['erreur' => 'Utilisateur non authentifié'], 401);
+        }
+
+        CommonUtilitary::LogUsedFunction(Auth::id(), "getCommonPasswords");
+
+        $url = 'https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10k-most-common.txt';
+        $response = Http::get($url);
+
+        $content = $response->body();
+        $passwords = explode("\n", $content);
+
+        $checkPassword = request()->password;
+
+        if (in_array($checkPassword, $passwords))
+        {
+            return response()->json(['message' => 'Le mot de passe fait partie des mots de passe les plus fréquents'], 200);
+        }
+        else 
+        {
+            return response()->json(['message' => 'Le mot de passe ne fait partie des mots de passe les plus fréquents'], 200);
+        }
     }
 }
